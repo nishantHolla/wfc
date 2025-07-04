313ac74d7b2356e80f10af10b3f55e8c3fb75712
Nishant Holla <nishantholla19@gmail.com>
2025-07-04 16:59:30 +0530

    Add wfc_directions

---
+++ src/include/wfc_directions.h
@@ -0,0 +1,19 @@
+#ifndef WFC_DIRECTIONS_H_
+#define WFC_DIRECTIONS_H_
+
+namespace wfc {
+
+enum class Directions {
+  NORTH,
+  NORTH_EAST,
+  EAST,
+  SOUTH_EAST,
+  SOUTH,
+  SOUTH_WEST,
+  WEST,
+  NORTH_WEST,
+};
+
+}
+
+#endif // !WFC_DIRECTIONS_H_
